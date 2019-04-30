ed46c874c16cdedeb0f5926b86704db38866c95c
Aurelio Amerio <aurelio.amerio@edu.unito.it>
2019-04-30 16:49:54 +0200

    up

--- lezione5/plot_es4.jl
+++ lezione5/plot_es4.jl
@@ -2,7 +2,7 @@ using DataFrames
 using CSV
 using Plots
 #%%
-file = open("./lezione5/Si_data.csv")
+file = open("./lezione5/Si_data_2.csv")
 df=CSV.read(file, header=false, delim=" ")
 
 plot(df[1],df[2])
