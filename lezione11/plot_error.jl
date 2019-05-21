f17972674f3a066cba4c33baed18c38a3ebd5b5c
Aurelio Amerio <aurelio.amerio@edu.unito.it>
2019-05-21 17:06:27 +0200

    plots

--- lezione11/plot_error.jl
+++ lezione11/plot_error.jl
@@ -10,11 +10,29 @@ data_eu = open("eu_error.dat")
 df_rk=CSV.read(data_rk, header=false, delim=" ")
 df_rk4=CSV.read(data_rk4, header=false, delim=" ")
 df_eu=CSV.read(data_eu, header=false, delim=" ")
-
 #%%
-plot(df_rk[1],df_rk[2], label="Runge-Kutta", dpi=600)
-plot!(df_rk4[1],df_rk4[2], label="RK 4th order", dpi=600)
-plot!(df_eu[1],df_eu[2], label="Euler", dpi=600)
+x_=4:2048
+eu=x_.^-1
+rk=x_.^-2
+rk4=x_.^-4
+#%%
+plot(df_rk[1],df_rk[2], label="Runge-Kutta", color="black", dpi=600)
+plot!(x_,rk, label="y=x^-2", color="black", line=(:dash))
 
-xaxis!(:log10, xlims=(1e-12,Inf))
-yaxis!(:log10, ylims=(1e-12,Inf))
+plot!(df_rk4[1],df_rk4[2], label="RK 4th order", color="red")
+plot!(x_,rk4, label="y=x^-4", color="red" , line=(:dash))
+
+plot!(df_eu[1],df_eu[2], label="Euler", color="blue")
+plot!(x_,eu, label="y=x^-1",color="blue", line=(:dash))
+
+
+
+
+
+xaxis!(:log10, xlims=(4,2048))
+yaxis!(:log10, ylims=(1e-15,Inf))
+title!("error as a function of n_steps")
+xlabel!("n_steps")
+ylabel!("error")
+plot!(legend=:bottom)
+savefig("errors.png")
